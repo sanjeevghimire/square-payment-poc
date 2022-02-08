328c08fb964f5f23aaff07a1b83375895425f85b
Sanjeev Ghimire <gsanjeev7@gmail.com>
2022-02-07 20:59:35 -0800

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { LoadScript } from './paymentForm'; 
+
+
+const applicationId=""
+const locationId=""
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Square Developer Offering</div>
+      <p>Payments API</p>
+      <LoadScript />
     </div>
   );
+ 
 }
 
 export default App;
