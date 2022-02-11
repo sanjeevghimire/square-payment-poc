96a545f509e43727e1141ceeb74f2649304f171c
Sanjeev Ghimire <gsanjeev7@gmail.com>
2022-02-11 10:44:18 -0800

    payment ui fixes

--- src/config/credentials.ts
+++ src/config/credentials.ts
@@ -1,5 +1,2 @@
-export const url:string = 'https://sandbox.web.squarecdn.com/v1/square.js'
-export const appId:string = "sandbox-sq0idb-R8971G96EGzUVMRrCKx5XQ"
-export const locationId:string = "LX4848WDYD7JM"
-
-export const hostname = "http://localhost:8080"
+export const appId:string = ""
+export const locationId:string = ''
